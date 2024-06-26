3188d1c6898a46667d3a60bd027aa226a9826bcd
Kacper Nalepa <kacper.nlp@wp.pl>
2024-06-26 23:54:01 +0200

    Styling section with buttons and headline for main/index view

--- app/index.tsx
+++ app/index.tsx
@@ -1,70 +1,25 @@
 import { useState } from "react";
-import { TextInput, View, Button, Text, StyleSheet, FlatList } from "react-native";
-import { Link } from 'expo-router';
-import { GoalElement, Goal, GoalStatus } from "@/components/GoalElement";
+import { View, Text, StyleSheet } from "react-native";
+import { Button } from 'react-native-paper';
 import globalStyles from '../styles/AppStyles'
 import appColors from '../styles/AppColors';
 
 export default function Index() {
-  const [goals, setGoals] = useState<Goal[]>([]);
-  const [text, setText] = useState('');
-
-  const handleChangeStatusOfGoal = (id: number) => {
-    const goalsList = [...goals];
-    goalsList.forEach(goal => {
-      if(goal.id === id) goal.status = goal.status === GoalStatus.Done ? GoalStatus.InProgress : GoalStatus.Done
-    })
-
-    setGoals(goalsList);
-  }
-
-  const handlePressAddNewGoal = () => {
-    const goal = {
-      id: Date.now(),
-      text,
-      status: GoalStatus.InProgress
-    }
-
-    setText('');
-    setGoals([...goals, goal]);
-  }
-
-  const doneGoals = goals.filter(goal => goal.status === GoalStatus.Done);
-  const inProgressGoals = goals.filter(goal => goal.status === GoalStatus.InProgress);
-
   return (
     <View style={styles.mainContainer}>
       <View style={styles.header}>
         <Text style={{...globalStyles.headline, textAlign: 'center'}}>Goal Tracker App</Text>
       </View>
       <View style={styles.body}>
-        <View style={styles.formSection}>
-          <TextInput style={globalStyles.input} placeholder="Your new goal..." value={text} onChangeText={newText => setText(newText)} />
-          <Button title="Add new" color={appColors.blue} onPress={handlePressAddNewGoal} /> 
+        <View style={styles.headlineBox}>
+          <Text style={styles.headlineText}>+Add a portion of water</Text>
         </View>
-        <View style={styles.goalsList}>
-          <FlatList 
-            data={doneGoals} 
-            keyExtractor={item => String(item.id)}
-            renderItem={({item}) => (
-              <View>
-                <GoalElement text={item.text} id={item.id} status={item.status} handlePressChangeStatus={handleChangeStatusOfGoal}  />
-              </View>
-            )}
-          >
-          </FlatList>
-        </View>
-        <View style={styles.goalsList}>
-          <FlatList 
-            data={inProgressGoals} 
-            keyExtractor={item => String(item.id)}
-            renderItem={({item}) => (
-              <View>
-                <GoalElement text={item.text} id={item.id} status={item.status} handlePressChangeStatus={handleChangeStatusOfGoal}  />
-              </View>
-            )}
-          >
-          </FlatList>
+        <View style={styles.buttonsBox}>
+          <View style={styles.innerButtonsBox}>
+            <Button icon="cup" mode="contained" buttonColor={appColors.blue}>Cup (250ml)</Button>
+            <Button icon="bottle-soda-classic-outline" mode="contained" buttonColor={appColors.blue}>Bottle (1L)</Button>
+          </View>
+          <Button icon="water-outline" mode="contained" buttonColor={appColors.blue}>Something else</Button>
         </View>
       </View>
     </View>
@@ -88,13 +43,22 @@ const styles = StyleSheet.create({
     marginHorizontal: 'auto',
     marginVertical: 20
   },
-  formSection: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 10
+  headlineBox: {
+    marginTop: 15,
+    marginBottom: 30
   },
-  goalsList: {
-    marginVertical: 20
+  headlineText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: appColors.white,
+    textAlign: 'center'
+  },
+  buttonsBox: {
+    gap: 20,
+    alignItems: 'center'
+  },
+  innerButtonsBox: {
+    flexDirection: 'row',
+    gap: 20
   }
 })
